88de2b337a3590f8fffd4ba72c1a0e57f1fadb10
Mertyce <mdoleman15@gmail.com>
2019-12-14 12:57:29 -0500

    updated templates and controllers

--- src/main/java/com/wellbrew/wellbrewapp/controller/OrderController.java
+++ src/main/java/com/wellbrew/wellbrewapp/controller/OrderController.java
@@ -3,21 +3,24 @@ package com.wellbrew.wellbrewapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("order")
 public class OrderController {
 
+    static ArrayList<String> orders = new ArrayList<>();
+
+    //use when the lists need to be pass to views - ProductController for more
+    /*static HashMap<String, String> orders = new HashMap<>();*/
+
     // request path: /order
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        ArrayList<String> orders = new ArrayList<>();
-        orders.add("August 2019");
-        orders.add("September 2019");
-        orders.add("October 2019");
 
         model.addAttribute("orders", orders);
         model.addAttribute("title", "Orders");
@@ -25,10 +28,23 @@ public class OrderController {
         return "order/index";
     }
 
-    @RequestMapping(value = "add")
+    // Request path: order/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddOrderForm(Model model) {
+
+
         model.addAttribute("title","Create Order");
         return"order/add";
+    }
+
+    //Request path: order/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddOrderForm(@RequestParam String orderName) {
+
+        orders.add(orderName);
+
+        // redirect to order/
+        return "redirect:";
     }
 }
 
--- src/main/java/com/wellbrew/wellbrewapp/controller/ProductController.java
+++ src/main/java/com/wellbrew/wellbrewapp/controller/ProductController.java
@@ -3,21 +3,25 @@ package com.wellbrew.wellbrewapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("product")
 public class ProductController {
 
-    // request path: /product
+    //arraylist used to pass information (ex. product.add)
+    /*static ArrayList<String> products = new ArrayList<>();*/
+
+    //hashmap used to store information (ex.product.put)
+    static HashMap<String, String> products = new HashMap<>();
+
+    // request path: product/
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        ArrayList<String> products = new ArrayList<>();
-        products.add("tea");
-        products.add("bear");
-        products.add("soda");
 
         model.addAttribute("products", products);
         model.addAttribute("title", "Products");
@@ -25,9 +29,45 @@ public class ProductController {
         return "product/index";
     }
 
-    @RequestMapping(value = "add")
+    // Request path: product/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddProductForm(Model model) {
-        model.addAttribute("title","Add New Product");
+
+        model.addAttribute("title","Add Product");
         return"product/add";
     }
+
+    // Request path: product/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddProductForm(@RequestParam String productName, @RequestParam String productDesc){
+
+        //previously added products listed within array WHEN INSIDE index function
+        /*products.add(productName);*/
+
+        //saving product
+        products.put(productName, productDesc);
+
+        //redirect to product/
+        return "redirect:";
+    }
+
+    //Request path: product/remove
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveProductForm(Model model) {
+        model.addAttribute("products", products.keySet());
+        model.addAttribute("title","Remove Product");
+        return "product/remove";
+    }
+
+    //Request path: product/remove
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveProductForm(@RequestParam ArrayList<String> product) {
+
+        for (String aProduct : product) {
+            products.remove(aProduct);
+        }
+
+        return "redirect:";
+    }
+
 }

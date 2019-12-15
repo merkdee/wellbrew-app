fdda380913a8c75fd56840954820be67481d3675
Mertyce <mdoleman15@gmail.com>
2019-12-14 19:20:01 -0500

    added models for controllers to use. added data class for model to use. adjusted templates to use data passed from controller via model.

--- src/main/java/com/wellbrew/wellbrewapp/model/Product.java
+++ src/main/java/com/wellbrew/wellbrewapp/model/Product.java
@@ -6,23 +6,55 @@ import java.util.Set;
 
 public class Product {
 
-    private int id;
+    private int productId;
+    private static int nextId = 1;
     private String name;
+    private int amount;
+    private String desc;
     private BigDecimal price;
     private boolean inStock;
-    private Set<Comment> comments = new LinkedHashSet<Comment>();
+    private String Vendor;
+    /*private Set<Comment> comments = new LinkedHashSet<Comment>();*/
 
-    public Product() {}
 
-    public Product(String name, BigDecimal price, boolean inStock, Set<Comment> comments) {
+    public Product(String name, int amount, String desc, BigDecimal price, boolean inStock, String vendor) {
+        this();
         this.name = name;
+        this.amount = amount;
+        this.desc = desc;
         this.price = price;
         this.inStock = inStock;
-        this.comments = comments;
+        this.Vendor = vendor;
     }
 
-    public int getId() {
-        return id;
+    public Product() {
+        productId = nextId;
+        nextId++;
+    }
+
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
     public String getName() {
@@ -49,11 +81,19 @@ public class Product {
         this.inStock = inStock;
     }
 
-    public Set<Comment> getComments() {
+    public String getVendor() {
+        return Vendor;
+    }
+
+    public void setVendor(String vendor) {
+        Vendor = vendor;
+    }
+
+/*    public Set<Comment> getComments() {
         return comments;
     }
 
     public void setComments(Set<Comment> comments) {
         this.comments = comments;
-    }
+    }*/
 }
